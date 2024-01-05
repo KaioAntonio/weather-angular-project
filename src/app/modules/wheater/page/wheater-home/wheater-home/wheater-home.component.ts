2d3417bce868e1d4dbd9cbd66ac5d9faa251ba8c
KaioAntonio <kaioantonio@bol.com.br>
2024-01-05 08:46:51 -0300

    add: search form

--- src/app/modules/wheater/page/wheater-home/wheater-home/wheater-home.component.ts
+++ src/app/modules/wheater/page/wheater-home/wheater-home/wheater-home.component.ts
@@ -1,25 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WheaterService } from '../../../services/wheater.service';
 import { WeatherDatas } from 'src/app/models/interfaces/WeatherDatas';
+import { Subject, takeUntil } from 'rxjs';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-wheater-home',
   templateUrl: './wheater-home.component.html',
   styleUrls: []
 })
-export class WheaterHomeComponent implements OnInit{
+export class WheaterHomeComponent implements OnInit, OnDestroy{
+  private readonly destroy$: Subject<void> = new Subject();
   initialCityName = 'Aracaju';
   wheaterDatas!: WeatherDatas;
+  searchIcon = faMagnifyingGlass;
 
   constructor(private weatherService: WheaterService) {
 
   }
+
   ngOnInit(): void {
     this.getWheaterDatas(this.initialCityName);
   }
 
   getWheaterDatas(cityName: string): void {
-    this.weatherService.getWheaterDatas(cityName)
+    this.weatherService
+    .getWheaterDatas(cityName)
+    .pipe(
+      takeUntil(this.destroy$)
+    )
     .subscribe({
       next: (response) => {
         response && (this.wheaterDatas = response);
@@ -27,6 +36,17 @@ export class WheaterHomeComponent implements OnInit{
       },
       error: (error) => console.log(error),
     })
+  }
+
+  onSubmit(): void {
+    this.getWheaterDatas(this.initialCityName);
+    this.initialCityName = '';
+
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
 }
