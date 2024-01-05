a47a48ead9d373bd624e9d56113d35a9b82da76c
KaioAntonio <kaioantonio@bol.com.br>
2024-01-05 09:02:21 -0300

    add: weather card component and input weather datas

--- src/app/modules/wheater/components/weather-card/weather-card.component.ts
+++ src/app/modules/wheater/components/weather-card/weather-card.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { WeatherDatas } from 'src/app/models/interfaces/WeatherDatas';
 
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
-  styleUrls: ['./weather-card.component.scss']
+  styleUrls: []
+
 })
-export class WeatherCardComponent {
+export class WeatherCardComponent implements OnInit {
+  @Input() weatherDatasInput!: WeatherDatas;
+
+  ngOnInit(): void {
+    console.log('DADOS RECEBIDOS DO PAPAI', this.weatherDatasInput)
+  }
 
 }
